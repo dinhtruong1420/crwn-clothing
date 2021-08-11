5dd63254c5cf2c58403277e71dfb23114148dd22
truong <vantruong01042000@gmail.com>
2021-08-11 15:18:03 +0700

    update app woth shop page, collection preview and item

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
 import { Switch, Route } from 'react-router-dom'
-const HatsPage = props => {
-  console.log(props)
-  return (
-    <div>
-      <h1>
-        HATS PAGE<br />
-        <button onClick={() => props.history.push('/')}>Hats</button>
-      </h1>
-    </div>
-  )
-}
+import ShopPage from './pages/shop/shop.component.jsx'
 function App () {
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
 
     </div>
